f51b2b4d762b23e2340efeddaf9c8a5b4e3799b9
CavInc <cavinc20@gmail.com>
2016-07-02 10:47:27 +0700

    Initial commit

---
+++ app/src/main/java/com/softdesign/devintensive/ui/activites/MainActivity.java
@@ -0,0 +1,17 @@
+package com.softdesign.devintensive.ui.activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.softdesign.devintensive.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
