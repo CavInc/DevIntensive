6a9d9f3e024cf4582b3b52cf56bc1d1bb6a1120e
CavInc <cavinc20@gmail.com>
2016-07-03 22:20:55 +0700

    Убраны не нужные иморты

--- app/src/main/java/com/softdesign/devintensive/ui/activites/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activites/MainActivity.java
@@ -1,12 +1,11 @@
 package com.softdesign.devintensive.ui.activites;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
