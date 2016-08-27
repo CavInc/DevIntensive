503c8f8eced1c60036b01a0fbd41614f0bdb65d2
CavInc <cavinc20@gmail.com>
2016-08-27 18:20:30 +0700

    Работа с RecycledView

--- app/src/main/java/com/softdesign/devintensive/ui/activites/UserListActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activites/UserListActivity.java
@@ -7,10 +7,13 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.data.managers.DataManager;
@@ -18,7 +21,6 @@ import com.softdesign.devintensive.data.network.res.UserListRes;
 import com.softdesign.devintensive.ui.adapters.UsersAdapter;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -48,6 +50,9 @@ public class UserListActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
         mRecyclerView = (RecyclerView) findViewById(R.id.user_list);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager (linearLayoutManager);
 
         setupToolbar();
         setupDrawer();
